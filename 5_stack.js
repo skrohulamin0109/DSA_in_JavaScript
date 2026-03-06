6fe35a00ea3363869e958a28e13a6ec53f091d28
Sk Rohul Amin <skrohulamin@0109@gmail.com>
2026-03-06 23:29:22 +0530

    add code to check the stack

--- 5_stack.js
+++ 5_stack.js
@@ -36,3 +36,13 @@ class Stack {
 //       The Constructor: In JavaScript, it is standard practice to use the new keyword when creating an error. While throw Error("...") technically works in most environments, new Error("...") is the conventional, explicit way to instantiate the object.
 
 //       The Object Type: JavaScript uses Error as the base class. While you can technically "throw" anything in JS (like a string or a number), using the Error object is best practice because it captures a stack trace, which helps you debug where the failure occurred.
+
+let stk = new Stack()
+// stk.pop()
+stk.push(10);
+stk.push(20);
+stk.push(30);
+// console.log(stk.peek())
+console.log(stk.pop());
+console.log(stk.pop());
+console.log(stk.pop());
