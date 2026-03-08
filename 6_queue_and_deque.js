2975450f3cb983c938e89f0b0e36c8d6d200ba80
Sk Rohul Amin <skrohulamin@0109@gmail.com>
2026-03-08 14:47:44 +0530

    enqueue and dequeue using pointer approach

---
+++ 6_queue_and_deque.js
@@ -0,0 +1,42 @@
+class Queue_pointer_approach {
+    constructor() {
+        this.items = [];
+        this.front = -1;
+        this.rear = -1;
+    }
+
+    isEmpty() {
+        if ( this.front === -1 || this.front > this.rear ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    enqueue(value) {
+        if (this.isEmpty()) {
+            this.front = 0;
+            this.rear = 0;
+        } else {
+            this.rear += 1;
+        }
+        this.items.push(value);
+    }
+
+    dequeue() {
+        if (this.isEmpty()) {
+            throw new Error("empty queue");
+        }
+        let remItem = this.items[this.front];
+        this.front += 1;
+
+        if (this.isEmpty()) {
+            this.items = [];
+            this.front = -1;
+            this.rear = -1;
+        }
+        return remItem;
+    }
+}
+
+
