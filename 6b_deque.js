53582d40fdd3412c215966e6854d4832110a87d4
Sk Rohul Amin <skrohulamin@0109@gmail.com>
2026-03-08 15:42:16 +0530

    seperate queue and dequeue codes in different file. Add dequeue shifting approach

---
+++ 6b_deque.js
@@ -0,0 +1,73 @@
+class Deque_pointer_approach {
+    constructor() {
+        this.items = [];
+        this.front = -1;
+        this.rear = -1;
+    }
+
+    isEmpty() {
+        if ( this.front === -1 || this.front > this.rear ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    enqueueAtBeg(value) {
+        if (this.isEmpty()) {
+            this.front = 0;
+            this.rear = 0;
+        } else {
+            this.rear += 1;
+        }
+        this.items.push(value);
+    }
+
+    dequeueAtEnd() {
+        if (this.isEmpty()) {
+            throw new Error("empty queue");
+        }
+        let remItem = this.items[this.front];
+        this.front += 1;
+
+        if (this.isEmpty()) {
+            this.items = [];
+            this.front = -1;
+            this.rear = -1;
+        }
+        return remItem;
+    }
+}
+
+
+class Deque_shifting_approach{
+  constructor(){
+    this.items = [];
+  };
+
+  isEmpty(){
+    return this.items.length === 0;
+  };
+
+  enqueueAtEnd(value){
+    this.items.push(value);
+  };
+
+  enqueueAtBeg(value){
+    return this.items.unshift(value);
+  }
+
+  dequeueAtBeg(){
+    if(this.isEmpty()){
+      throw new Error("empty deque")
+    }
+    return this.items.shift();
+  }
+
+  dequeueAtEnd(){
+  if(this.isEmpty()){
+    throw new Error("empty deque")
+  }
+  return this.items.pop();
+  }
+}
