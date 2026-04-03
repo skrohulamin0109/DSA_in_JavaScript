2668bf98ce3dd4a97d9502e6e6f65b1847aa5bd1
Sk Rohul Amin <skrohulamin@0109@gmail.com>
2026-04-03 07:20:19 +0530

    add fixed sliding window examples to 26_sliding_window.js file

--- 26_sliding_window.js
+++ 26_sliding_window.js
@@ -24,32 +24,69 @@ function lenOflngstStr(str) {
 
 // Longest subarray with sum < s
 
-function longestSubarrayWithSumLessThanS(arr, s){
-  const n = arr.length;
-  let l = -1; // left pointer
-  let curr = 0;
-  let best = 0;
+function longestSubarrayWithSumLessThanS(arr, s) {
+    const n = arr.length;
+    let l = -1; // left pointer
+    let curr = 0;
+    let best = 0;
 
-  for (let r = 0; r < n; r++){
-    curr += arr[r];
+    for (let r = 0; r < n; r++) {
+        curr += arr[r];
 
-    while(curr >= s){
-      l++;
-      curr -= arr[l];
+        while (curr >= s) {
+            l++;
+            curr -= arr[l];
+        }
+
+        best = Math.max(best, r - 1);
     }
-
-    best = Math.max(best, r-1)
-  }
 }
-
 
 //------------------------------------FIXED SLIDING WINDOW--------------
 
 // Maximum Sum of Subarray of size K
 
-function maxSumOfSubarrayOfSizeK(arr, k){
-  const n = arr.length
+function maxSumOfSubarrayOfSizeK(arr, k) {
+    const n = arr.length;
+    // let curr = 0;
+    // let best = 0;
+    // for (let i = 0; i < k; i++) {
+    //     curr = curr + arr[i];
+    //     best = best + arr[i];
+    // }
 
+    // array.reduce((accumulator, current) => newValue, initialValue)
+
+    let initial = arr
+        .slice(0, n)
+        .reduce((accumulator, current) => accumulator + current, 0); // 0 is the intial value
+    let curr = (best = initial);
+
+    for (let r = k; r < n; r++) {
+        curr = curr + arr[r] - arr[r - k];
+
+        best = Math.max(best, curr);
+    }
 }
 
+//Max average subarray l. You have an integer array nums consisting of n elements and an integer k. Find a contigious subarray whose length is equal to k that has the maximum average value an dreturn this value. Any answer with a calculation error less than 10^-5 will be accepted.
 
+function findmaxAvg(nums, k) {
+    const n = nums.length;
+    let curr_sum = 0;
+
+    for (let i = 0; i < k; i++) {
+        curr_sum += nums[i];
+    }
+
+    let max_avg = curr_sum / k;
+    let avg;
+    for (let i = k; i < n; i++) {
+        curr_sum = curr_sum + nums[i] - nums[i - k];
+
+        avg = curr_sum / k;
+        max_avg = Math.max(max_avg, avg);
+    }
+
+    return max_avg;
+}
